d0e5c251dc2cd9df00631338960ee6ee1671e8da
ivan schlagheck <ivansch05@gmail.com>
2021-11-16 12:26:03 +0100

    created orgamparams setorgans functions on private and show

--- app/controllers/organs_controller.rb
+++ app/controllers/organs_controller.rb
@@ -1,2 +1,16 @@
 class OrgansController < ApplicationController
+  before_action :set_organ, only: [:show, :edit, :update, :destroy]
+
+  def show
+    
+  end
+
+  private
+  def organ_params
+    params.require(:organ).permit(:name, :description)
+  end
+
+  def set_organ
+    @organ = Organ.find(params[:id])
+  end
 end
