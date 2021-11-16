f9b46a87a8cd87d5475a6353858500b1d544be4d
Damian Beissel <damian.beissel@googlemail.com>
2021-11-16 16:31:13 +0100

    booking form not linked yet to show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,21 @@
 class BookingsController < ApplicationController
-    resources :bookings, only [ :new, :create ]
+  def new
+    @booking = Booking.new
+    @organ = Organ.find(params[:organ_id])
+  end
+  def create
+    @organ = Organ.find(params[:organ_id])
+    @booking = Booking.new(booking_params)
+    @booking.user_id = 24
+    @booking.organ = @organ
+    @booking.save
+  end
+
+  private
+
+  def booking_params
+    params.require(:start_on).permit(:ends_at)
+  end
 end
+
+# current_user.id
