d3ae01449b9a9acb796f396a4c8b78ba8f691c8b
ameliedarenberg <amelie.arenberg@icloud.com>
2021-11-16 11:41:24 +0100

    trying to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+puts "destroying"
+
 User.create(
   username: 'donaldtrump',
   password: 'blablabla',
@@ -40,3 +36,4 @@ User.create(
   password: "ihateclowns",
   blood_type: "O+"
 )
+puts "users created"
