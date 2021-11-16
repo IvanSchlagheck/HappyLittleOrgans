7b3a9dd900d8419ba44bf488d48e9ff123666965
ivan schlagheck <ivansch05@gmail.com>
2021-11-16 13:03:20 +0100

    views working ugly but working

--- app/controllers/organs_controller.rb
+++ app/controllers/organs_controller.rb
@@ -2,9 +2,11 @@ class OrgansController < ApplicationController
   before_action :set_organ, only: [:show]
 
   def show
+    
   end
 
   private
+  
   def organ_params
     params.require(:organ).permit(:name, :description)
   end
