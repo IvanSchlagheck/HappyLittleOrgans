7b9e805e8f7028a95822ea0cc0910e31aa796459
ameliedarenberg <amelie.arenberg@icloud.com>
2021-11-16 10:55:44 +0100

    clean

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  username: 'donaldtrump',
+  password: 'blablabla',
+  blood_type: 'A+'
+)
+
+User.create(
+  username: 'mickeymouse',
+  password: 'disney',
+  blood_type: 'O+'
+)
+
+User.create(
+  username: 'minniemouse',
+  password: "mickey",
+  blood_type: "B-"
+)
+
+User.create(
+  username: "bambi",
+  password: "hunter",
+  blood_type: "A+"
+)
+
+User.create(
+  username: "winniethepooh",
+  password: "honey",
+  blood_type: "AB+"
+)
+
+User.create(
+  username: "Ivan",
+  password: "ihateclowns",
+  blood_type: "O+"
+)
