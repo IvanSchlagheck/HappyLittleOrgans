13692dc084f06bdc607f4d66f02e052401212766
ameliedarenberg <amelie.arenberg@icloud.com>
2021-11-16 11:46:42 +0100

    seeded and added device, all working

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,36 +4,42 @@ puts "destroying"
 User.create(
   username: 'donaldtrump',
   password: 'blablabla',
-  blood_type: 'A+'
+  blood_type: 'A+',
+  email: "fakeemail6@email.com"
 )
 
-User.create(
+User.create!(
   username: 'mickeymouse',
   password: 'disney',
-  blood_type: 'O+'
+  blood_type: 'O+',
+  email: "fakeemail5@email.com"
 )
 
 User.create(
   username: 'minniemouse',
   password: "mickey",
-  blood_type: "B-"
+  blood_type: "B-",
+  email: "fakeemail4@email.com"
 )
 
 User.create(
   username: "bambi",
   password: "hunter",
-  blood_type: "A+"
+  blood_type: "A+",
+  email: "fakeemail3@email.com"
 )
 
 User.create(
   username: "winniethepooh",
   password: "honey",
-  blood_type: "AB+"
+  blood_type: "AB+",
+  email: "fakeemail2@email.com"
 )
 
 User.create(
   username: "Ivan",
   password: "ihateclowns",
-  blood_type: "O+"
+  blood_type: "O+",
+  email: "fakeemail1@email.com"
 )
 puts "users created"
