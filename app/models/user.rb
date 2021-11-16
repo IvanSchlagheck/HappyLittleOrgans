7f7b5c0a71a5d154cf9f12eeae531b271321a777
Damian Beissel <damian.beissel@googlemail.com>
2021-11-16 14:30:23 +0100

    add validation and conection

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :bookings
+  has_many :bookings, through: :organs
   has_many :organs
   has_many :organs, through: :bookings
   validates :username, presence: true, uniqueness: true
