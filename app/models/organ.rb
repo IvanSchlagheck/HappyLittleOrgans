6229e5fc4f679b785efbe6766b52743ac02eae9d
Marc Rodenbusch <marcrodenbusch@gmail.com>
2021-11-17 14:48:54 +0100

    update index page

--- app/models/organ.rb
+++ app/models/organ.rb
@@ -1,6 +1,7 @@
 class Organ < ApplicationRecord
   belongs_to :user
-  has_many :bookings 
+  has_many :bookings
   validates :name, presence: true
   validates :description, presence: true
+  has_one_attached :photo
 end
