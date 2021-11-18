1cd3ddce6e4f6cd24a97c2c0d2df6f39838a9e96
ivan schlagheck <ivansch05@gmail.com>
2021-11-18 12:36:37 +0100

    updated the organ model so it destroy the depensant booking when an organ gets destroyed

--- app/models/organ.rb
+++ app/models/organ.rb
@@ -1,6 +1,7 @@
 class Organ < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependant: :destroy
+  
   validates :name, presence: true
   validates :description, presence: true
   has_one_attached :photo
